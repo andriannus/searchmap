93f03637810ce671a525ddec4ef96954a47abfd2
Andre <andriansimamora@gmail.com>
2018-07-05 22:00:10 +0700

    Set timezone to Jakarta

--- application/config/config.php
+++ application/config/config.php
@@ -488,7 +488,7 @@ $config['compress_output'] = FALSE;
 | helper' page of the user guide for information regarding date handling.
 |
 */
-$config['time_reference'] = 'local';
+$config['time_reference'] = 'Asia/Jakarta';
 
 /*
 |--------------------------------------------------------------------------
