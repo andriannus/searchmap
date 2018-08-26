239d835929645c28fbd7be24166e120bb0fb33c0
Andre <andriansimamora@gmail.com>
2018-08-26 22:45:53 +0700

    Update SQL file

--- searchmap.sql
+++ searchmap.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2018 at 09:53 PM
--- Server version: 10.1.33-MariaDB-1~xenial
--- PHP Version: 7.2.5-1+ubuntu16.04.1+deb.sury.org+1
+-- Generation Time: Aug 26, 2018 at 10:45 PM
+-- Server version: 10.1.35-MariaDB-1~xenial
+-- PHP Version: 7.2.8-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -53,6 +53,21 @@ CREATE TABLE `tb_guestbookarea` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `avatar` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -70,6 +85,12 @@ ALTER TABLE `tb_guestbookarea`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -77,13 +98,19 @@ ALTER TABLE `tb_guestbookarea`
 -- AUTO_INCREMENT for table `tb_guestbook`
 --
 ALTER TABLE `tb_guestbook`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_guestbookarea`
 --
 ALTER TABLE `tb_guestbookarea`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
