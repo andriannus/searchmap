382523026b2994860995319b83993802fff962c8
Andre <andriansimamora@gmail.com>
2018-09-26 10:37:41 +0700

    Change CSS Framework to UIkit

--- application/views/core/layouts/app.php
+++ application/views/core/layouts/app.php
@@ -31,9 +31,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="icon" type="image/png" href="<?= base_url('assets/images/fav.png'); ?>"/>
 
   <!-- Load asset CSS dan JavaScript. Koneksi internet diperlukan -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.16/css/uikit.min.css" />
+
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+  <!-- UIkit JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.16/js/uikit.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.16/js/uikit-icons.min.js"></script>
 
   <script src="<?= base_url('assets/js/wow.min.js'); ?>"></script>
   <script src="https://unpkg.com/vue@2.5.17/dist/vue.min.js"></script>
@@ -114,9 +120,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   // Jalankan fungsi ketika user melakukan scroll
   window.onscroll = () => showBtnToTop();
-
-  // Default untuk menggunakan Wow.js
-  wow = new WOW().init();
   </script>
 </body>
 </html>
